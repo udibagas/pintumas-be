de6e2ad1e21cc3d43a579f564df91736811fc4dc
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-08-22 21:13:22 +0700

    implement gate and searching

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,15 +5,30 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index($request)
     {
-        $data = Post::with(['user', 'comments'])->paginate();
+        $data = Post::with(['user', 'comments', 'department'])
+            ->when(
+                $request->input('user_id'),
+                fn($query) => $query->where('user_id', $request->input('user_id'))
+            )
+            ->when(
+                $request->input('department_id'),
+                fn($query) => $query->where('department_id', $request->input('department_id'))
+            )
+            ->when(
+                $request->input('search'),
+                fn($query) =>
+                $query->where('title', 'ilike', '%' . $request->input('search') . '%')
+            )
+            ->paginate();
         return response()->json($data);
     }
 
@@ -22,6 +37,7 @@ class PostController extends Controller
      */
     public function store(StorePostRequest $request)
     {
+        Gate::authorize('create', Post::class);
         return response()->json(Post::create($request->validated()), 201);
     }
 
@@ -38,6 +54,7 @@ class PostController extends Controller
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
+        Gate::authorize('update', $post);
         $post->update($request->validated());
         return response()->json($post);
     }
@@ -47,6 +64,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        Gate::authorize('delete', $post);
         $post->delete();
         return response()->json(null, 204);
     }
